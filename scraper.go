9dbe0d68ad6cb316a7cb588497f44f653631de77
Alex <AlexGrimes.se@gmail.com>
2022-11-01 18:03:26 -0400

    initial commit

---
+++ scraper.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/gocolly/colly"
+)
+
+type item struct {
+	Name   string `json:"name"`
+	Price  string `json:"price"`
+	ImgUrl string `json:"imgurl"`
+}
+
+func main() {
+	c := colly.NewCollector()
+
+	c.OnHTML("", func(h *colly.HTMLElement) {
+
+	})
+}
